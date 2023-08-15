b6a0f8ae8cbe1964781089c503db845711dd81cb
MuhammadFahri <fahribl2014@gmail.com>
2023-08-15 11:04:50 +0700

    Integrating Context and Reducer

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,24 @@
-
+import { useEffect, useState } from "react";
+import { useCart } from "../context/CartContext";
 import "./ProductCard.css";
 
 export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
+  const { cartList, addToCart, removeFromCart } = useCart();
+  const [isInCart, setIsInCart] = useState(false);
+
+  const {id, name, price, image} = product;
+
+  useEffect(() => {
+    const productIsInCart = cartList.find(cartItem => cartItem.id === id);
+
+    if(productIsInCart){
+      setIsInCart(true);
+    } else {
+      setIsInCart(false);
+    }
+
+  }, [cartList, id]);
+
 
   return (
     <div className="productCard">
@@ -10,7 +26,7 @@ export const ProductCard = ({product}) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+        { isInCart ? (<button className="remove" onClick={() => removeFromCart(product)}>Remove</button>) :  (<button onClick={() => addToCart(product)}>Add To Cart</button>) }
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CartProvider } from "./context/CartContext";
 import './index.css';
 import App from './App';
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </Router>
   </React.StrictMode>
 );
